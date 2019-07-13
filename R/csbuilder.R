0c0fd92ecaf2badc900c4ea469679af6fc655b5c
Naike Wang <wangnaike1989@gmail.com>
2019-07-13 10:54:16 -0500

    update csbuilder

--- R/csbuilder.R
+++ R/csbuilder.R
@@ -6,7 +6,7 @@ csbuilder <- function() {
   function.name <- readline(prompt="Name your function: ")
   function.des <- readline(prompt="Describe function in a sentence: ")
   Rdcreator <- paste("#'", function.des)
-  while(TRUE) {
+
     function.body <-  readline(prompt="Write function format: ")
     template <- writeLines(paste(Rdcreator, '\n',
                                  function.name,"<- function(){", '\n',
@@ -14,7 +14,7 @@ csbuilder <- function() {
                                  "cat(bold$blue('\\n|", function.des,"\\n\\n'))",'\n',
                                  "cat(bold$red('Example:\\n'),",
                                  "'\\t ",function.body, "\\n\\n')",'\n',
-                                 "# INSERT EXTRA EXAMPLE CODE IF APPLICABLE",'\n',
+                                 "# INSERT EXTRA EXAMPLE CODE IF APPLICABLE #",'\n',
                                  "cat(rep('\\n', 3))",'\n',
                                  "ask_csbuilder()",'\n',
                                  "}",'\n',
@@ -22,18 +22,27 @@ csbuilder <- function() {
                                  "# and run the following command IN CONSOLE to generate .Rd file for this function:",'\n',
                                  "# devtools::document()",
                                  sep = ""))
-    if(readline("Type Y to continue adding examples. Type any other key to re-enter information.") == 'Y')
-      break}
-  while(TRUE){
-    extra <- readline(prompt="Write function format: ")
-    writeLines(paste("cat(bold$red('Example:\\n'),",
-                     "'\\t ",extra, "\\n\\n')", sep = ""))
-    if(readline("Type N to stop. Type any other key to continue adding examples.") == 'N')
-      break
-  }
-  cat(rep("\n", 3))
-  ask_csbuilder()
+    cat(rep("\n", 5))
+    cat(bold$cyan('Do you want to add more examples?'))
+      switch(
+           menu(c("Continue adding examples", "Go back to main menu", "I'm done.")),
+           extra(),
+           go(),
+           exit())
+
 }
 
+extra <- function(){
+{
 
-
+  example <- readline(prompt="Write function format: ")
+  writeLines(paste("cat(bold$red('Example:\\n'),",
+                   "'\\t ",example, "\\n\\n')", sep = ""))
+  cat(rep("\n", 5))
+  cat(bold$cyan('Do you want to add more examples?'))
+  switch(menu(c("Continue adding examples", "Go back to main menu", "I'm done.")),
+         extra(),
+         go(),
+         exit())
+}
+}

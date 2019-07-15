183ab0f8530fe284d968ad37c1488e4cb4fce375
Naike Wang <wangnaike1989@gmail.com>
2019-07-14 20:29:30 -0500

    gadgetbuilder()

---
+++ R/gadgetbuilder.R
@@ -0,0 +1,26 @@
+
+#' This function is used to build ask_pagename gadget
+#' @export
+
+# note that you need to give function.des() a value
+
+gadgetbuilder <- function(){
+  pagename <- readline(prompt="Name the page: ")
+  writeLines(paste(
+    "#'", function.des,"\n",
+    "#'@export\n",
+    "ask_",pagename," <- function(){", '\n\t',
+    "cat(bold$cyan('Need more help?'))",'\n\t',
+    "switch(menu(c(",c("'Sure! Go back to previous menu!'"),",\n\t",
+    c("\"Sure! Go back to main menu!\""),",\n\t",
+    c("\"I'm good.\""), ")),\n\t",
+    pagename,"(),\n\t",
+    "go(),\n\t",
+    "exit())",
+    "\n\t}",
+    sep = ""))
+}
+
+function.des <- c("THIS IS A TEST")
+
+
